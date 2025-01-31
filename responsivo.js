6dc243b24ed87cf91488357c5ec491e68d48bc23
Gabriel Sousa <104863987+gabrisousa@users.noreply.github.com>
2025-01-31 16:24:00 -0300

    Add files via upload

---
+++ responsivo.js
@@ -0,0 +1,219 @@
+function ajustarVideoIntro() {
+    let video = document.getElementById("introVideo");
+    
+    if (window.innerWidth < 768) {
+      video.src = "./src/videos/video-intro-responsivo.mp4";
+    } else {
+      video.src = "./src/videos/video-intro.mp4";
+    }
+  }
+
+
+  window.addEventListener("resize", ajustarVideoIntro);
+window.addEventListener("load", ajustarVideoIntro);
+
+
+
+
+function ajustarVideoAfton() {
+    let video = document.getElementById("jumpscareAfton");
+    
+    if (window.innerWidth < 768) {
+      video.src = "./src/videos/video-afton-responsivo.mp4";
+    } else {
+      video.src = "./src/videos/video-afton.mp4";
+    }
+  }
+
+
+  window.addEventListener("resize", ajustarVideoAfton);
+window.addEventListener("load", ajustarVideoAfton);
+
+
+
+
+function ajustarImagemTransição() {
+    let imagem = document.getElementById("transição");
+    
+    if (window.innerWidth < 768) {
+      imagem.src = "./src/imagens/transição-responsivo.png";
+    } else {
+      imagem.src = "./src/imagens/transição.png";
+    }
+  }
+
+
+  window.addEventListener("resize", ajustarImagemTransição);
+window.addEventListener("load", ajustarImagemTransição);
+
+
+
+function ajustarVideoFreddy() {
+  let video = document.getElementById("jumpscareFreddy");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/video-freddy-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/video-freddy.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoFreddy);
+window.addEventListener("load", ajustarVideoFreddy);
+
+
+function ajustarVideoChica() {
+  let video = document.getElementById("jumpscareChica");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/video-chica-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/video-chica.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoChica);
+window.addEventListener("load", ajustarVideoChica);
+
+
+function ajustarVideoBonnie() {
+  let video = document.getElementById("jumpscareBonnie");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/video-bonnie-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/video-bonnie.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoBonnie);
+window.addEventListener("load", ajustarVideoBonnie);
+
+
+function ajustarVideoFoxy() {
+  let video = document.getElementById("jumpscareFoxy");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/video-foxy-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/video-foxy.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoFoxy);
+window.addEventListener("load", ajustarVideoFoxy);
+
+
+function ajustarVideoGolden() {
+  let video = document.getElementById("jumpscareGolden");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/video-golden-freddy-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/video-Golden-freddy.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoGolden);
+window.addEventListener("load", ajustarVideoGolden);
+
+
+
+function ajustarVideoNightmareFreddy() {
+  let video = document.getElementById("jumpscareNightmareFreddy");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumpscare-nightmare-freddy-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-nightmare-freddy.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoNightmareFreddy);
+window.addEventListener("load", ajustarVideoNightmareFreddy);
+
+
+
+function ajustarVideoNightmareChica() {
+  let video = document.getElementById("jumpscareNightmarechica");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumpscare-nightmare-chica-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-nightmare-chica.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoNightmareChica);
+window.addEventListener("load", ajustarVideoNightmareChica);
+
+
+
+
+function ajustarVideoNightmareBonnie() {
+  let video = document.getElementById("jumpscare-nightmare-bonnie");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumpscare-nightmare-bonnie-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-nightmare-bonnie.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoNightmareBonnie);
+window.addEventListener("load", ajustarVideoNightmareBonnie);
+
+
+
+function ajustarVideoNightmareFoxy() {
+  let video = document.getElementById("jumpscare-nightmare-foxy");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumpscre-nightmare-foxy-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-nightmare-foxy.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoNightmareFoxy);
+window.addEventListener("load", ajustarVideoNightmareFoxy);
+
+
+
+function ajustarVideoFredbear() {
+  let video = document.getElementById("jumpscare-fredbear");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumpscare-fredbear-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-fredbear.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoFredbear);
+window.addEventListener("load", ajustarVideoFredbear);
+
+
+function ajustarVideoNightmarionne() {
+  let video = document.getElementById("jumpscare-nightmarionne");
+  
+  if (window.innerWidth < 768) {
+    video.src = "./src/videos/jumspcare-nightmarionne-responsivo.mp4";
+  } else {
+    video.src = "./src/videos/jumpscare-nightmarionne.mp4";
+  }
+}
+
+
+window.addEventListener("resize", ajustarVideoNightmarionne);
+window.addEventListener("load", ajustarVideoNightmarionne);
